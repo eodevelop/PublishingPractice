ee7dc1a417e0f784597b51eb9b78784a813d07cf
minjae3292 <minjae3292@tradlinx.com>
2021-07-10 16:48:51 +0900

    1. 기본 레이아웃 구성 및 폴더 구성

--- 6/index.js
+++ 6/index.js
@@ -1,29 +1,36 @@
 function showAbout() {
-    $('.About').show();
-    $('.Studio').hide();
-    $('.Works').hide();
-    $('.Contact').hide();
+    $('#About').show();
+    $('#Studio').hide();
+    $('#Works').hide();
+    $('#Contact').hide();
 }
 
 function showStudio() {
-    $('.About').hide();
-    $('.Studio').show();
-    $('.Works').hide();
-    $('.Contact').hide();
+    $('#About').hide();
+    $('#Studio').show();
+    $('#Works').hide();
+    $('#Contact').hide();
 }
 
 function showWorks() {
-    $('.About').hide();
-    $('.Studio').hide();
-    $('.Works').show();
-    $('.Contact').hide();
+    $('#About').hide();
+    $('#Studio').hide();
+    $('#Works').show();
+    $('#Contact').hide();
 }
 
 function showContact() {
-    $('.About').hide();
-    $('.Studio').hide();
-    $('.Works').hide();
-    $('.Contact').show();
+    $('#About').hide();
+    $('#Studio').hide();
+    $('#Works').hide();
+    $('#Contact').show();
 }
 
 showAbout();
+
+$(document).ready(function () {
+    $("#About").load("./page/about.html");
+    $("#Studio").load("./page/studio.html");
+    $("#Works").load("./page/Works.html");
+    $("#Contact").load("./page/Contact.html");
+});
