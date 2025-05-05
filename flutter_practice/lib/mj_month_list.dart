4bd30b25e3bdba722bd8006a09250e57e4a17aae
eodevelop <eodevelop@naver.com>
2025-05-05 15:22:33 +0900

    프로젝트 재 세팅 완료

---
+++ flutter_practice/lib/mj_month_list.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MjMonthList());
+}
+
+class MjMonthList extends StatelessWidget {
+  const MjMonthList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
